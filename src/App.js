82d8dd048307fae1c573d32aaa21faefc188fb85
Pawel Chmielewski <panpawelchmielewski@gmail.com>
2024-03-02 17:39:34 +0100

    Add controlled elements in split bill component

--- src/App.js
+++ src/App.js
@@ -157,26 +157,46 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
   return (
     <form className="form-split-bill">
       <h2>Split Bill with {selectedFriend.name}</h2>
 
       <label>💲 Bill value</label>
-      <input type="text"></input>
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      ></input>
 
       <label>😎 Your expenses</label>
-      <input type="text"></input>
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+      ></input>
 
-      <label>👩🏻‍🤝‍🧑🏻 XYZ expense</label>
-      <input type="text" disabled></input>
+      <label>👩🏻‍🤝‍🧑🏻 {selectedFriend.name}`s expense</label>
+      <input type="text" disabled value={paidByFriend}></input>
 
       <label>Who is paying the bill</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
-        <option value="friend">XYZ</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
 
       <Button>Split bill</Button>
     </form>
   );
 }
+/* Now add math! */
